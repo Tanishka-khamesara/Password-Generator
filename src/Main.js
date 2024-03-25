791d41f4371bd06769e99c8bb5363062bc1a684f
Tanishka khamesara <tanishkajain24941@gmail.com>
2024-03-25 22:29:11 +0530

    code updated

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 
 function Main() {
@@ -7,15 +7,16 @@ function Main() {
     const [lowercase, setLowercase] = useState(false);
     const [special, setSpecial] = useState(false);
     const [number, setNumber] = useState(false);
+    const[Password,setPassword]=useState()
 
 
-    const generatePassword = (() => {
+    const generatePassword = useCallback(() => {
         let str = "";
         let passout = "";
         if (length==0 || length<8 ||length>50) {
             alert("please enter length in range")
         }
-        else if (uppercase == false || lowercase == false || number == false || special == false) {
+        else if (uppercase == false && lowercase == false && number == false && special == false) {
             alert("please select atleast one checkbox");
         }
 
@@ -24,15 +25,21 @@ function Main() {
         if (special) str += "!@#$%^&*()_+=-{}|[]\\:;\"'<>,.?/";
         if (number) str += "0123456789";
 
-        
+        for (let i = 0; i < length; i++){
+            const Randomindex = Math.floor(Math.random()*str.length);
+            // console.log(Randomindex);
+            passout += str.charAt(Randomindex);
+        }
+        console.log(passout);
+        setPassword(passout);
 
 
 
-    })
+    },[length,uppercase,lowercase,number])
     return (
         <div>
             <h1>Password Generator</h1>
-            <div class="output"></div>
+            <div class="output">{Password}</div>
             <div style={{display:"flex", justifyContent:"space-between" }}>
                 <p>Select Password length(**8-50 characters**)</p>
                 <input type="number" onChange={(e) => {
